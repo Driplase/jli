6c20d1fc2959f17b94e87e08fd5fb8d2b55fef23
iEzp <120309121+iEzp@users.noreply.github.com>
2024-03-10 21:49:10 +0900

    Id -> ClassName

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const l = location.hostname;
-document.title = `${document.title} | ${l}`
+document.title = `${document.title} | ${l}`;
 
 if (l === "j.hide.li") {
     document.getElementById("o0").innerHTML = null;
     document.getElementById("logo").src = "/assets/logo/jhideli.png";
-    document.getElementById("logo").style.width = 120;
+    document.getElementById("logo").style.width = "120px";
 }
 
 var spas = document.getElementById("ctj");
