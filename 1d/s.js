493b25657b5a3021ac5bfd6dd93742e9a99412dc
Driplase <85550737+Driplase@users.noreply.github.com>
2024-05-06 23:09:59 +0900

    number

--- 1d/s.js
+++ 1d/s.js
@@ -1,10 +1,11 @@
 var qer = document.location.search.substring(1);
-var prmt = qer.split('=');var d = 100;
+var prmt = qer.split('=');
+var d = 100;
 var n = 100;
 var r;
 if (decodeURIComponent(prmt[0]) === "n") {
-    document.getElementById("ti").textContent = `1d${qt[0]}`;
-    n = qt[0];
+    document.getElementById("ti").textContent = `1d${prmt[1]}`;
+    n = prmt[1];
     console.log(`customized! 1d${n}`);
 } else {
     console.log("default! 1d100");
